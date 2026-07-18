1ec87a27bb8678677a8c500f7ff347fe2628c025
Satyajit Pratihar <satyajitpratihar96@gmail.com>
2026-07-18 22:41:20 +0530

    Update ESLint config to simplify file extensions
    Removed unnecessary extends from ESLint configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -7,13 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
+    files: ['**/*.{js,jsx
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
